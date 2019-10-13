695bddbd363fa230db0d734ee6b2f56fb6eaca3e
Rainer Sabelfeld <rainer.sabelfeld@gmail.com>
2019-10-13 09:27:21 +0200

    transfering Assignment2 from local repo to GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##makeCacheMatrix <- function(x = matrix()) {
+##
+##}
 
+makeCacheMatrix <- function(X = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        X <<- Y
+        inv <<- NULL
+    }
+    get <- function() X
+    setInverse <- function(solve) inv <<- solve
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve <- function(x, ...) {
+##        ## Return a matrix that is the inverse of 'x'
+##}
+
+cacheSolve <- function(X, ...) {
+    inv <- X$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- X$get()
+    inv <- solve(data, ...)
+    X$setInverse(inv)
+    inv
 }
